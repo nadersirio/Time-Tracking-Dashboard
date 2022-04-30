d6ac66b83188afb23922fae689435a4c6aeea9b1
nadersirio <nadersirio@gmail.com>
2022-04-29 21:49:06 -0300

    scriptTest

---
+++ scriptTest.js
@@ -0,0 +1,33 @@
+function dayClick() {
+  let day = document.getElementById("day");
+  if (true) {
+    console.log("day");
+    day.disabled = true;
+  }
+  week.disabled = false;
+  month.disabled = false;
+  return;
+}
+
+function weekClick() {
+  let week = document.getElementById("week");
+  if (true) {
+    console.log("week");
+    week.disabled = true;
+
+  }
+  day.disabled = false;
+  month.disabled = false;
+  return;
+}
+
+function monthClick() {
+  let month = document.getElementById("month");
+  if (true) {
+    console.log("month");
+    month.disabled = true;
+  }
+  day.disabled = false;
+  week.disabled = false;
+  return;
+}
